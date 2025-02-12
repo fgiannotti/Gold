17b590eb8ba37aa656f97b09b8f538434d04c363
jmgiannotti <juanmartingiannotti@gmail.com>
2025-02-11 22:30:19 -0300

    print fix

--- player/Player.gd
+++ player/Player.gd
@@ -6,7 +6,7 @@ signal food_updated(value: float)
 const SPEED = 180
 
 const run_speed = 1000
-var hp := 10.0
+var hp := 20.0
 var food: float
 const STEPS_FOR_HUNGER = 100
 var step_count = 0
@@ -103,7 +103,7 @@ func direction_string(direction: Vector2):
 func recive_damage(dmg: float):
 	if dmg < self.hp:
 		self.hp -= dmg
-		print("Player recieved damage: ", dmg, "HP: ", hp)
+		print("Player recieved damage: ", dmg, " HP: ", hp)
 	else: 
 		print("Dead X(")
 	
