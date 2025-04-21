4bf6c7474be8e2b8ab0daa426ef6ce4cda23a5d4
EmicAssets <e.calligo@gmail.com>
2025-04-21 18:32:36 -0300

    added carbs and sugar
    added the energy bar as a starter snack

--- inventory/items/energy_bar.gd
+++ inventory/items/energy_bar.gd
@@ -1,8 +1,8 @@
-class_name EnergyBar extends InventoryItem
+class_name RedApple extends InventoryItem
 
 @export var recover_value: int
 
 func consume(player: Player) -> bool:
 	PlayerManager.set_food(PlayerManager.food + recover_value)
-	print('[EnergyBar] consuming Energy Bar')
+	print('[RedApple] consuming apple')
 	return true
