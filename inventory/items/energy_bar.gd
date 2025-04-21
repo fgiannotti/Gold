37d243923cb16e1354af918f57e08ee32a68ee35
EmicAssets <e.calligo@gmail.com>
2025-04-21 18:48:03 -0300

    apple and energy bar fix
    fixed the mix after duplication of apple.gd to make an energy bar

--- inventory/items/energy_bar.gd
+++ inventory/items/energy_bar.gd
@@ -1,8 +1,8 @@
-class_name RedApple extends InventoryItem
+class_name EnergyBar extends InventoryItem
 
 @export var recover_value: int
 
 func consume(player: Player) -> bool:
 	PlayerManager.set_food(PlayerManager.food + recover_value)
-	print('[RedApple] consuming apple')
+	print('[EnergyBar] consuming Energy Bar')
 	return true
